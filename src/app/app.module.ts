f19caf0dc7fcefac5099cbc6d92ea3840d9554a4
Romario Ribeiro <romaryjane@hotmail.com>
2021-04-06 21:25:39 -0300

    projeto 3 iniciado na linguagem angular alunos: Romario Ribeiro e Matheus Pastori

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadComponent } from './combonets/head/head.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BordaComponent } from './combonets/borda/borda.component';
+import { NavComponent } from './combonets/nav/nav.component';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    BordaComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
